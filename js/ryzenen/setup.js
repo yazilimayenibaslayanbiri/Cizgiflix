25bda78503922325a614f3b03f3854820ea264aa
Ryzenen <87452249+ryzenen@users.noreply.github.com>
2022-06-06 22:55:57 +0300

    Çizgiflix

---
+++ js/ryzenen/setup.js
@@ -0,0 +1,21 @@
+$('.ryzenen-carousel').ryzenenCarousel({
+    dots: false,
+    stagePadding: 50,
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:4500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
